f7a26bfb6d070b8d5f16b3315e6ee5270548d675
Katellyn Hyker <llyn00@yahoo.com>
2026-01-15 09:54:24 -0500

    Create tabs for journal app

--- week1/codingEx/App.tsx
+++ week1/codingEx/App.tsx
@@ -1,12 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+type RootParamList = {
+  Home: undefined;
+  Journal: undefined;
+  NewEntry: undefined;
+};
+
+const Tab = createBottomTabNavigator<RootParamList>();
+
+
+function HomeTab() {
+  return(
+  <View style={styles.container}>
+    <Text style={styles.title}>Home</Text>
+  </View>
+  );
+}
+
+function JournalTab() {
+  return(
+  <View style={styles.container}>
+    <Text style={styles.title}>Journal</Text>
+  </View>
+  );
+}
+
+function NewEntryTab() {
+  return(
+  <View style={styles.container}>
+    <Text style={styles.title}>New Entry</Text>
+  </View>
+  );
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeTab} />
+        <Tab.Screen name="Journal" component={JournalTab} />
+        <Tab.Screen name="NewEntry" component={NewEntryTab} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +56,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
